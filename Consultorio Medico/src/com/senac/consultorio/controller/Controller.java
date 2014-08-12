1198bcaeddfacb0634c0e5af3335a95e9c7bea37
Dantieris Castilhos Rabelini <dantieris@gmail.com>
2014-08-12 13:46:17 -0300

    Add implementacao da classe teste, e ajuste de identecao.

--- Consultorio Medico/src/com/senac/consultorio/controller/Controller.java
+++ Consultorio Medico/src/com/senac/consultorio/controller/Controller.java
@@ -6,6 +6,7 @@ import com.senac.consultorio.model.Consulta;
 import com.senac.consultorio.model.Medicamento;
 import com.senac.consultorio.model.Paciente;
 import com.senac.consultorio.model.Tipo;
+import com.senac.consultorio.teste.CadastrosTeste;
 import com.senac.consultorio.view.Console;
 import com.senac.consultorio.view.Menu;
 
@@ -14,14 +15,31 @@ public class Controller {
 	private ArrayList<Paciente> pacientes;
 	private ArrayList<Medicamento> medicamentos;
 	private ArrayList<Consulta> agendaConsultas;
-	
 	private ArrayList<Consulta> registroConsultas;
 	
 	public Controller() {
-		pacientes 		= new ArrayList<Paciente>();
-		medicamentos	= new ArrayList<Medicamento>();
-		agendaConsultas = new ArrayList<Consulta>();
-		this.registroConsultas = new ArrayList<Consulta>();
+		this.pacientes 			= new ArrayList<Paciente>();
+		this.medicamentos		= new ArrayList<Medicamento>();
+		this.agendaConsultas 	= new ArrayList<Consulta>();
+		this.registroConsultas 	= new ArrayList<Consulta>();
+		
+		inserirCadastroTeste();
+	}
+	
+	private void inserirCadastroTeste() {
+		CadastrosTeste ct = new CadastrosTeste();
+		
+		for (Paciente p : ct.pacientesCadastrados()) {
+			pacientes.add(p);
+		}
+		
+		for (Medicamento m : ct.medicamentosCadastrados()) {
+			medicamentos.add(m);
+		}
+		
+		for (Consulta c : ct.consultasAgendadas(pacientes)) {
+			agendaConsultas.add(c);
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -38,12 +56,26 @@ public class Controller {
 		int op = Menu.exibirMenu();
 		
 		switch(op) {
-		case 1 : exibirCadastrarPaciente(); break;
-		case 2 : exibirCadastrarMedicamento(); break;
-		case 3 : exibirAgendamentoConsulta(); break;
-		case 4 : exibirRegistrarConsulta(); break;
-		case 5 : exibirHistorico(); break;
-		case 6 : Console.exibirEncerramento(); System.exit(0);
+		case 1 : 
+			exibirCadastrarPaciente(); 
+			break;
+		case 2 : 
+			exibirCadastrarMedicamento(); 
+			break;
+		case 3 : 
+			exibirAgendamentoConsulta(); 
+			break;
+		case 4 : 
+			exibirRegistrarConsulta(); 
+			break;
+		case 5 : 
+			exibirHistorico(); 
+			break;
+		case 6 : 
+			Console.exibirEncerramento(); 
+			System.exit(0);
+			break;
+		default : Menu.exibirMensagem("Opção Inválida!");
 		}
 	}
 	
@@ -81,10 +113,16 @@ public class Controller {
 
 	private Tipo receberTipo(int tipo) {
 		switch(tipo) {
-		case 1 : return Tipo.ORAL;
-		case 2 : return Tipo.INTRA_VENOSO;
-		case 3 : return Tipo.CAPSULA;
-		case 4 : return Tipo.INTRA_MUSCULAR;
+		case 1 : 
+			return Tipo.ORAL;
+		case 2 : 
+			return Tipo.INTRA_VENOSO;
+		case 3 : 
+			return Tipo.CAPSULA;
+		case 4 : 
+			return Tipo.INTRA_MUSCULAR;
+		default : 
+			Menu.exibirMensagem("Opção Inválida!");
 		}
 		return null;
 	}
@@ -150,7 +188,10 @@ public class Controller {
 		int cont = 1;
 		
 		for (Paciente paciente : pacientes) {
-			ms.append(cont++ +" - " + paciente.getNome() + "\n");
+			ms.append(cont++ + 
+					" - " + 
+					paciente.getNome() + 
+					"\n");
 		}
 		
 		Menu.exibirMensagem(ms.toString());
@@ -179,14 +220,17 @@ public class Controller {
 		int cont = 1;
 		
 		for (Medicamento medicamento : medicamentos) {
-			ms.append(cont++ + " - " + medicamento.getNome() + "\n");
+			ms.append(cont++ + 
+					" - " + 
+					medicamento.getNome() + 
+					"\n");
 		}
 		Menu.exibirMensagem(ms.toString());
 		int retorno = Menu.receberNumeroMedicamento();
 		if(retorno == 0){
 			return 0;
 		}else{
-		return (retorno - 1);
+			return (retorno - 1);
 		}
 	}
 	
@@ -206,8 +250,12 @@ public class Controller {
 		int cont = 1;
 		
 		for (Consulta consulta : agendaConsultas) {
-			ms.append(cont++ + " - Data: " + consulta.getData() + " - Hora: " + consulta.getHorario() + "\n");
+			ms.append(cont++ + 
+					" - Data: " + consulta.getData() + 
+					" - Hora: " + consulta.getHorario() + 
+					"\n");
 		}
+		
 		Menu.exibirMensagem(ms.toString());
 		return (Menu.receberNumeroConsulta() - 1);
 	}
---
+++ Consultorio Medico/src/com/senac/consultorio/teste/CadastrosTeste.java
@@ -0,0 +1,61 @@
+package com.senac.consultorio.teste;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.senac.consultorio.model.Consulta;
+import com.senac.consultorio.model.Medicamento;
+import com.senac.consultorio.model.Paciente;
+import com.senac.consultorio.model.Tipo;
+
+public class CadastrosTeste {
+	public List<Paciente> pacientesCadastrados() {
+		List<Paciente> pacientes = new ArrayList<Paciente>();
+		
+		Paciente joao = new Paciente("João", 21, "5123568956", "Rua Andradas, Nº 3214", 03123056345);
+		Paciente jose = new Paciente("José", 21, "5123568956", "Rua Andradas, Nº 3214", 03123056345);
+		Paciente pedro = new Paciente("Pedro", 21, "5123568956", "Rua Andradas, Nº 3214", 03123056345);
+		Paciente luiz = new Paciente("Luiz", 21, "5123568956", "Rua Andradas, Nº 3214", 03123056345);
+		Paciente maria = new Paciente("Maria", 21, "5123568956", "Rua Andradas, Nº 3214", 03123056345);
+		
+		pacientes.add(pedro);
+		pacientes.add(jose);
+		pacientes.add(maria);
+		pacientes.add(luiz);
+		pacientes.add(joao);
+		
+		return pacientes;
+	}
+	
+	public List<Medicamento> medicamentosCadastrados() {
+		List<Medicamento> medicamentos = new ArrayList<Medicamento>();
+		
+		Medicamento neosoro = new Medicamento("Neosoro", "12345698", Tipo.ORAL, "benzalconio + clor.sodio + nafazolina");
+		Medicamento salonpas = new Medicamento("Salonpas" , "12345692", Tipo.ORAL, "canfora+mentol+salicilato metila");
+		Medicamento buscopan = new Medicamento("Buscopan", "12345678", Tipo.CAPSULA, "escopolamina");
+		Medicamento rivotril = new Medicamento("Rivotril", "12345629", Tipo.CAPSULA, "clonazepam");
+				
+		medicamentos.add(neosoro);
+		medicamentos.add(rivotril);
+		medicamentos.add(buscopan);
+		medicamentos.add(salonpas);
+		
+		return medicamentos;
+	}
+	
+	public List<Consulta> consultasAgendadas(List<Paciente> pacientes) {
+		List<Consulta> consultas = new ArrayList<Consulta>();
+		
+		Consulta consulta1345 = new Consulta(pacientes.get(0), 1245, 13112014);
+		Consulta consulta1530 = new Consulta(pacientes.get(1), 1530, 13112014);
+		Consulta consulta1000 = new Consulta(pacientes.get(2), 1000, 12112014);
+		Consulta consulta1615 = new Consulta(pacientes.get(4), 1615, 12112014);
+		
+		consultas.add(consulta1615);
+		consultas.add(consulta1000);
+		consultas.add(consulta1530);
+		consultas.add(consulta1345);
+		
+		return consultas;
+	}
+}
